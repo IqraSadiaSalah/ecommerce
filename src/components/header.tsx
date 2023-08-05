a452b1087af78453ffd0caed2d53e0b0a2cfdce7
IqraSadiaSalah <iqrasadia505@gmail.com>
2023-08-05 17:46:37 +0500

    commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -143,7 +143,7 @@ export function Header() {
 
                 <NavigationMenuItem className="pl-10">
                   <Link href={"/cart"} onClick={() => setNavbar(false)}>
-                    <button className="my-3 md:my-0 relative bg-gray-200 text-black hover:bg-gray-300 hover:text-black rounded-full p-2.5">
+                    <button className="my-3 md:my-2 relative bg-gray-200 text-black hover:bg-gray-300 hover:text-black rounded-full p-2.5">
                       <AiOutlineShoppingCart className="text-xl" />
                       {cartValue != 0 && (
                         <span className="absolute top-0.5 -right-2 bg-[#F02D34] text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
