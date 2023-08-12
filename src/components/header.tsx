7b4fd245212ac2ce2537141b0aebf34f3df50ad2
IqraSadiaSalah <iqrasadia505@gmail.com>
2023-08-12 22:22:30 +0500

    committed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -47,8 +47,8 @@ export function Header() {
   }, [dispatch, user_id]);
 
   return (
-    <div className="w-full">
-      <NavigationMenu className="pt-8">
+    <div className="w-full pl-14">
+      <NavigationMenu className="pt-8 ">
         <div className="px-6 md:items-center md:flex w-full md:w-fit justify-between">
           <div className="flex items-center py-3">
             <div className="w-full">
@@ -63,7 +63,7 @@ export function Header() {
                     className="p-2  text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
                     onClick={() => setNavbar(!navbar)}
                   >
-                     {navbar ? (
+                    {navbar ? (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="w-6 h-6 text-black"
@@ -98,13 +98,13 @@ export function Header() {
             </div>
           </div>
 
-         <div>
+          <div>
             <div
               className={`h-[90vh] md:h-fit w-full flex justify-center items-center pb-4 mt-4 md:block md:pb-0 md:mt-0 ${
                 navbar ? "block" : "hidden"
               }`}
             >
-              <NavigationMenuList className="flex-col md:flex-row px-10 flex-shrink">
+              <NavigationMenuList className="flex-col md:flex-row px-10 flex-shrink pl-10">
                 <NavigationMenuItem>
                   <Link href={"/female"} onClick={() => setNavbar(false)}>
                     <p className="my-1 md:my-0">Female</p>
@@ -112,27 +112,26 @@ export function Header() {
                 </NavigationMenuItem>
                 <NavigationMenuItem>
                   <Link href={"/male"} onClick={() => setNavbar(false)}>
-                    <p className="my-1 md:my-0">Male</p>
+                    <p className="my-1 md:my-0 pl-4">Male</p>
                   </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
                   <Link href={"/kids"} onClick={() => setNavbar(false)}>
-                    <p className="my-1 md:my-0">Kids</p>
+                    <p className="my-1 md:my-0 pl-4">Kids</p>
                   </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
                   <Link href={"/products"} onClick={() => setNavbar(false)}>
-                    <p className="mt-1 mb-2 md:my-0">All Products</p>
+                    <p className="mt-1 mb-2 md:my-0 pl-4">All Products</p>
                   </Link>
                 </NavigationMenuItem>
-                <NavigationMenuItem>
+                <NavigationMenuItem className="pl-8">
                   <div className="flex border rounded-lg border-black h-8 items-center">
                     <Button className="bg-white text-black hover:bg-white hover:text-black h-6 pr-1.5 mr-0">
                       <BiSearch />
                     </Button>
                     <Input
                       type="text"
-                      placeholder="What you looking for"
                       className="border-none h-6"
                       onChange={(e) => {
                         push(`/search?name=${e.target.value}`);
@@ -141,7 +140,7 @@ export function Header() {
                   </div>
                 </NavigationMenuItem>
 
-                <NavigationMenuItem>
+                <NavigationMenuItem className="pl-10">
                   <Link href={"/cart"} onClick={() => setNavbar(false)}>
                     <button className="my-3 md:my-0 relative bg-gray-200 text-black hover:bg-gray-300 hover:text-black rounded-full p-2.5">
                       <AiOutlineShoppingCart className="text-xl" />
@@ -154,7 +153,7 @@ export function Header() {
                   </Link>
                 </NavigationMenuItem>
 
-                <NavigationMenuItem>
+                <NavigationMenuItem className="pl-10">
                   {user ? (
                     <button
                       className="underline"
@@ -179,5 +178,3 @@ export function Header() {
     </div>
   );
 }
-
-//let it be commited again
